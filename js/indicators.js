49d7d26b66a67b8b9d4e98ea7c565465879086aa
bcampbell <ben@scumways.com>
2011-10-10 16:45:53 +0100

    add some test indicators and highlighting

---
+++ js/indicators.js
@@ -0,0 +1,56 @@
+
+
+indicators = {
+    'study': [/a study has found/i, /new \w+ research/i,
+        /tested on rats/i,
+        /research suggests/i
+ ],
+
+    // anonymous sources from inkstainedwretch.com:
+    // http://inkstainedwretch.com/public/examples/anonymous.php.txt
+    'anon_source': [ 
+        /did not wish to be identified/i,
+        /did not want to be identified/i,
+        /declined to be identified/i,
+        /refused to be identified/i,
+        /an anonymous source/i,
+        /An anonymous source/i,
+        /asked not to be identified/i,
+        /declined to give her name/i,
+        /declined to give his name/i,
+        /on condition of anonymity/i,
+        /requested anonymity/i,
+        /asked that his name not be used/i,
+        /asked that her name not be used/i,
+        /refused to give her name/i,
+        /refused to give his name/i,
+        /sources close to/i,
+        /a source close to/i,
+        /A source close to/i,
+        /iasked not to be named/i,
+        /ideclined to be named/i,
+        /irefused to be named/i,
+        /iwouldn't give his name/i,
+        /iwouldn't give her name/i,
+        /ispoke on background/i,
+        /ispeaking on background/i,
+        /ispoke off the record/i,
+        /ispeaking off the record/i,
+        /ispeak off the record/i,
+        /icomment off the record/i,
+        /iwould not speak for attribution/i,
+        /ideclined to speak for attribution/i,
+        /irefused to speak for attribution/i,
+        /iasked to remain anonymous/i,
+        /ithe source said/i,
+        /ia source said/i,
+        /isources said/i,
+        /iaccording to people familiar with/i,
+        /ian official close to/i,
+        /ia person briefed on the matter/i,
+        /iinsisted on anonymity/i,
+        /ichose to remain anonymous/i
+    ]
+};
+
+
