a381d0bc4798f566d45836767f6dda8e1cf23c57
Ben Campbell <ben@scumways.com>
2012-06-12 16:43:41 +1200

    separate front-page display for logged-in users

--- sourcy/handlers/front.py
+++ sourcy/handlers/front.py
@@ -53,53 +53,88 @@ class DailyBreakdown(BaseHandler):
 
 
 
+class DailySummary(object):
+    """ helper for X% percent complete by day """
+    def __init__(self, session, day):
+        self.day = day
+        self.total = session.query(Article).\
+            filter(cast(Article.pubdate, Date) == day).\
+            count()
+
+        self.sourced = session.query(Article).\
+            filter(cast(Article.pubdate, Date) == day).\
+            filter(Article.needs_sourcing==False).\
+            count()
+
+        if self.total>0:
+            self.percent_sourced = (100*self.sourced) / self.total
+        else:
+            self.percent_sourced = 0
+
+
+
+
+
 class FrontHandler(BaseHandler):
     def get(self):
 
-        # some articles which need sourcing
-        random_arts = self.session.query(Article).\
-            options(subqueryload(Article.tags,Article.sources,Article.comments)).\
-            filter(Article.needs_sourcing==True).\
-            order_by(func.rand()).\
-            limit(4).all()
 
-        recent_actions = self.session.query(Action).\
-            filter(Action.what.in_(('src_add','art_add','mark_sourced','mark_unsourced','helpreq_open','helpreq_close'))).\
-            order_by(Action.performed.desc()).slice(0,10)
 
+        #TODO: top sourcers
         all_users = self.session.query(UserAccount).slice(0,10).all()
         top_sourcers = [random.choice(all_users) for i in range(8)]
 
-        daily = daily_breakdown(self.session)[:7]
-
-
-
-        class DailySummary(object):
-            def __init__(self, session, day):
-                self.day = day
-                self.total = session.query(Article).\
-                    filter(cast(Article.pubdate, Date) == day).\
-                    count()
-
-                self.sourced = session.query(Article).\
-                    filter(cast(Article.pubdate, Date) == day).\
-                    filter(Article.needs_sourcing==False).\
-                    count()
-
-                if self.total>0:
-                    self.percent_sourced = (100*self.sourced) / self.total
-                else:
-                    self.percent_sourced = 0
 
         today_summary = DailySummary(self.session, datetime.datetime.utcnow().date())
 
-        self.render('front.html',
-            random_arts = random_arts,
-            recent_actions = recent_actions,
-            daily = daily,
-            groupby = itertools.groupby,
-            top_sourcers = top_sourcers,
-            today_summary = today_summary)
+
+        if self.current_user:
+            today = datetime.datetime.utcnow().date() - datetime.timedelta(days=1)
+
+            unsourced_arts = self.session.query(Article).\
+                filter(cast(Article.pubdate, Date) >= today).\
+                filter(Article.needs_sourcing == True).\
+                all()
+
+            helpreq_arts = self.session.query(Article).\
+                filter(Article.help_reqs.any()).\
+                order_by(Article.pubdate.desc())
+
+
+            recent_actions = self.session.query(Action).\
+                filter(Action.what.in_(('src_add','art_add','mark_sourced','mark_unsourced','helpreq_open','helpreq_close'))).\
+                order_by(Action.performed.desc()).slice(0,10)
+
+
+            self.render('front_loggedin.html',
+                unsourced_arts = unsourced_arts,
+                helpreq_arts = helpreq_arts,
+                top_sourcers = top_sourcers,
+                today_summary = today_summary,
+                recent_actions = recent_actions)
+
+
+        else:
+            recent_actions = self.session.query(Action).\
+                filter(Action.what.in_(('src_add','art_add','mark_sourced','mark_unsourced','helpreq_open','helpreq_close'))).\
+                order_by(Action.performed.desc()).slice(0,10)
+
+            # some articles which need sourcing
+            random_arts = self.session.query(Article).\
+                options(subqueryload(Article.tags,Article.sources,Article.comments)).\
+                filter(Article.needs_sourcing==True).\
+                order_by(func.rand()).\
+                limit(4).all()
+
+            self.render('front.html',
+                random_arts = random_arts,
+                recent_actions = recent_actions,
+                groupby = itertools.groupby,
+                top_sourcers = top_sourcers,
+                today_summary = today_summary)
+
+
+
 
 
 
