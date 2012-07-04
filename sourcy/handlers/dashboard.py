9cb0cb2257084129bdc42b8c30356f4776678c5b
Ben Campbell <ben@scumways.com>
2012-07-05 11:54:03 +1200

    tidy up user profile page and dashboard

--- sourcy/handlers/dashboard.py
+++ sourcy/handlers/dashboard.py
@@ -10,45 +10,65 @@ from sqlalchemy.sql.expression import cast,func
 from sqlalchemy.orm import subqueryload
 
 from base import BaseHandler
-from sourcy.models import Article,Action,Lookup,Tag,TagKind,UserAccount,Comment,article_tags
+from sourcy.models import Article,Action,Lookup,Tag,TagKind,UserAccount,Comment,article_tags,comment_user_map
+
+
 
 class DashboardHandler(BaseHandler):
 
     @tornado.web.authenticated
     def get(self):
 
-        #TODO: top sourcers
-        all_users = self.session.query(UserAccount).all()
-        top_sourcers = [random.choice(all_users) for i in range(12)]
-
-
-#        today_summary = DailySummary(self.session, datetime.datetime.utcnow().date())
-
-
-        # logged-in "dashboard" version of front page
         today = datetime.datetime.utcnow().date() - datetime.timedelta(days=1)
 
         unsourced_arts = self.session.query(Article).\
             filter(cast(Article.pubdate, Date) >= today).\
             filter(Article.needs_sourcing == True).\
+            order_by(Article.pubdate.desc()).\
             all()
 
         helpreq_arts = self.session.query(Article).\
             filter(Article.help_reqs.any()).\
-            order_by(Article.pubdate.desc())
+            order_by(Article.pubdate.desc()).\
+            limit(5)
 
 
-        recent_actions = self.session.query(Action).\
-            filter(Action.what.in_(('src_add','art_add','mark_sourced','mark_unsourced','helpreq_open','helpreq_close'))).\
-            order_by(Action.performed.desc()).slice(0,6)
+
+        # find actions performed by other people on articles this user has touched
+        arts_of_interest = self.session.query(Action.article_id).\
+            distinct().\
+            filter(Action.article_id != None).\
+            filter(Action.user==self.current_user)
+
+        actions_of_interest = self.session.query(Action).\
+            filter(Action.article_id.in_(arts_of_interest)).\
+            filter(Action.user!=self.current_user).\
+            order_by(Action.performed.desc()).\
+            limit(10)
+
+
+#            filter(Action.what.in_(('src_add','art_add','mark_sourced','mark_unsourced','helpreq_open','helpreq_close'))).\
+#           order_by(Action.performed.desc()).slice(0,6)
+
+        subq = self.session.query(comment_user_map.c.comment_id).\
+            filter(comment_user_map.c.useraccount_id==self.current_user.id).\
+            subquery()
+        recent_mentions = self.session.query(Action).\
+            options(subqueryload(Action.comment,Action.article,Action.user)).\
+            filter(Action.what=='comment').\
+            filter(Action.comment_id.in_(subq)).\
+            order_by(Action.performed.desc()).\
+            slice(0,10).\
+            all()
 
 
         self.render('dashboard.html',
             unsourced_arts = unsourced_arts,
             helpreq_arts = helpreq_arts,
-            top_sourcers = top_sourcers,
-            today_summary = today_summary,
-            recent_actions = recent_actions)
+            actions_of_interest = actions_of_interest,
+            recent_mentions = recent_mentions)
+        
+
 
 
 
