476ce778d21d5748b6dd313e742c1cc1e9779f0b
Ben Campbell <ben@scumways.com>
2012-09-04 15:29:32 +1200

    add labels to api/lookup + other tweaks for plugin

--- unsourced/handlers/api.py
+++ unsourced/handlers/api.py
@@ -1,33 +1,35 @@
+import urllib
 
 import tornado.web
-
-
 
 from base import BaseHandler
 from unsourced.models import Article,ArticleURL
 from unsourced import util
-
 from unsourced import config
+
 
 class LookupHandler(BaseHandler):
     """ api to look up an article by url
 
-    returns article details as json, or 404 if not found
+    returns article details as json
     """
 
     def get(self):
-        urls = util.www_or_not(self.get_argument('url'))
+        lookup_url = self.get_argument('url')
+        urls = util.www_or_not(lookup_url)
         q = self.session.query(ArticleURL.article_id).\
             filter(ArticleURL.url.in_(urls))
         arts = self.session.query(Article).\
             filter(Article.id.in_(q)).all()
 
         if len(arts) == 0:
-            raise tornado.web.HTTPError(404, "Article not found")
+            submit_url = config.settings.root_url + '/addarticle?' + urllib.urlencode({'url':lookup_url})
+            self.finish({'status': 'not_found', 'submit_url': submit_url})
+            return
 
         art = arts[0]
 
-        unsourced_url = config.settings.root_url + '/' +art.art_url()
+        unsourced_url = config.settings.root_url + art.art_url()
 
         sources = []
         
@@ -42,13 +44,27 @@ class LookupHandler(BaseHandler):
 
             sources.append(s) 
 
-        details = {'headline': art.headline,
+        labels = []
+        for l in art.labels:
+            labels.append( dict(
+                id = l.label.id,
+                prettyname = l.label.prettyname,
+                description = l.label.description,
+                icon_url = config.settings.root_url + l.label.icon_url('m') 
+            ) )
+
+        details = {
+            'status': 'found',
+            'headline': art.headline,
             'permalink': art.permalink,
             'pubdate': art.pubdate.isoformat(),
             'other_urls': [url.url for url in art.urls if url.url != art.permalink],
             'unsourced_url' : unsourced_url,
+            'summary_url' : unsourced_url,  # TODO: do a proper summary page!
             'sources' : sources,
+            'labels': labels,
             'needs_sourcing': art.needs_sourcing }
+
         self.finish(details)
 
 
